235a0e618219e77374530266ae014eb2eeb26208
Nidzoki <nikolavidovic01@gmail.com>
2024-01-15 16:39:59 +0100

    Sada radi i registracija

--- reg_prij_pokusaj/registracija.js
+++ reg_prij_pokusaj/registracija.js
@@ -1,34 +1,51 @@
-import express from 'express'
-//const express = require("express")
+import express from 'express';
+
 const app=express()
+
 app.use(express.json())
-//var obavijest
-function prvoProvjera(ime, prezime, mail, lozinka, potvrda_lozinke)
-{
-    (req,rep)=>(app.fetch(`/users/searchByMail/${mail}`)
-    .then(rep=rep.json())
-    .then(trazeniMail=rep.mail)
-    .catch(err=>console.log(err)))
 
-    if (trazeniMail!=null) {
-        uvjet1=false
-    }
+async function userExists(mail)
+{   
+    const user = await fetch(`http://localhost:8080/users/searchByMail/${mail}`)
+    .then(response => response.json())
+    .catch(error => console.error('Error:', error))
 
-    if (ime, prezime, mail, lozinka, potvrda_lozinke != null && potvrda_lozinke===lozinka){
-        uvjet2=false
-    }
-
-    return (uvjet1 && uvjet2)
+    return (user !== undefined)
 }
-export default function registracijaKorisnika(ime, prezime, mail, lozinka, potvrda_lozinke)
-{ 
+export default async function registracijaKorisnika(name, surname, mail, password, repeatPassword, birthday)
+{   
+    let theme = "a" // ne implementiramo pa cemo koristiti ovo
     var boolPrijava=false
-    if(prvoProvjera(ime, prezime, mail, lozinka, potvrda_lozinke))
-    {
-        //sta je s rodendanom i temom neman pojma
-        //nisan 100% sigurna kako integrirat enkripciju za lozinku -> pogledat promjene na server.js
-        app.fetch(`/users`,{ime, prezime, mail, lozinka})
+    if(! await userExists(mail) && password === repeatPassword)
+    {   console.log("we in")
+       
+    const payload = {
+        "name":name,
+        "surname": surname,
+        "mail": mail,
+        "password": password,
+        "birthday": birthday,
+        "theme": theme
+    }
+
+    
+    const user = await fetch(`http://localhost:8080/users`,
+        {
+            method: 'POST',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+              },
+            body: JSON.stringify(payload)
+        } )
+        .then(response => response.json())
+        .then(data => console.log(JSON.stringify(data)))
+        .catch(error => console.error('Error:', error))
+        
+        console.log(user)
         boolPrijava=true
     }
     return boolPrijava
 }
+
+console.log(await registracijaKorisnika("Luka", "Lukic", "treciluka@nesto.com", "lukas pass", "lukas pass", "2024-01-22T23:00:00.000Z"))
